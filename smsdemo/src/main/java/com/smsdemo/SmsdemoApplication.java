591606c2939dd94bcbfe6f9affb929ce601b869f
saikec22 <151397407+saikec22@users.noreply.github.com>
2024-03-25 16:38:32 +0530

    Initial commit

---
+++ smsdemo/src/main/java/com/smsdemo/SmsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.smsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsdemoApplication.class, args);
+	}
+
+}
---
+++ smsdemo/src/test/java/com/smsdemo/SmsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
